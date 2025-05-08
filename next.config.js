b4f889cab2faa2cf19c48998555cff7b4d2818ba
Valerie <=>
2025-05-07 17:11:11 -0700

    Add security headers to Next.js configuration for enhanced protection
    - Updated `next.config.js` to include security headers that enforce CORS policies, improving application security and ensuring proper access control.
    - This change enhances the overall security posture of the application by restricting resource sharing and allowing only specified origins and methods.

--- next.config.js
+++ next.config.js
@@ -16,7 +16,33 @@ const nextConfig = {
     // Explicitly set the port for the Next.js server
     serverRuntimeConfig: {
       port: parseInt(process.env.PORT, 10) || 3000,
-    }
+    },
+    // Add security headers
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: 'https://saas-production-a504.up.railway.app',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'Content-Type, Authorization',
+            },
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+          ],
+        },
+      ];
+    },
   }
   
   module.exports = nextConfig
